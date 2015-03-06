d674d5922d25e538bfc04cfe2abb062f28afe75a
csharpwizard <tysoncoder@gmail.com>
2015-03-06 10:16:40 +0000

    Updated the pre and post batch scripts to use the TF_BUILD_BUILDDIRECTORY environment variable

--- src/BatchFiles/SonarPreBuild.bat
+++ src/BatchFiles/SonarPreBuild.bat
@@ -2,7 +2,11 @@
 
 @set SonarRunnerProperties=%1
 @set ProjectKey=%2
+
+@REM Set the config folder to the command line parameter, if supplied
 @set ConfigFolder=%3
+@if "%ConfigFolder%"=="" set ConfigFolder=%TF_BUILD_BUILDDIRECTORY%\Sonar\Config\
+@echo ConfigFolder = %ConfigFolder%
 
 @echo Sonar runner properties location: %SonarRunnerProperties%
 @echo Sonar project key: %ProjectKey%
@@ -11,9 +15,6 @@
 @echo Creating the Sonar config folder...
 @rmdir %ConfigFolder% /S /Q
 @mkdir %ConfigFolder%
-
-@echo Dumping environment variables... (TODO - REMOVE)
-@~dp0\DumpEnvironmentVars.exe
 
 @echo Generating Sonar FxCop file...
 @%~dp0\Sonar.FxCopRuleset.exe "%SonarRunnerProperties%" "%ProjectKey%" "%ConfigFolder%\SonarAnalysis.ruleset"
